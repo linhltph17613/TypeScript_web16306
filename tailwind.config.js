a1c2ee513f25e0860d073e716a7424c645f61b4f
linhltph17613 <linhlethuy1911@gmail.com>
2022-03-22 00:11:52 +0700

    feature banner

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["index.html", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: them => ({
+        'header-img': "url('https://i.imgur.com/5G0J8T4.jpg')",
+        'home-banner': "url('https://i.imgur.com/U7UVfPa.jpg')"
+      })
+    },
+  },
+  plugins: [],
+}
