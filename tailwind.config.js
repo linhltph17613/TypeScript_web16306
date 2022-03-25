cd53e7db43078dfd31eadbb6fce974124cec6dbc
linhltph17613 <linhlethuy1911@gmail.com>
2022-03-26 00:08:11 +0700

    feat: dd

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,9 @@ module.exports = {
     extend: {
       backgroundImage: them => ({
         'header-img': "url('https://i.imgur.com/5G0J8T4.jpg')",
-        'home-banner': "url('https://i.imgur.com/U7UVfPa.jpg')"
+        'home-banner': "url('https://i.imgur.com/U7UVfPa.jpg')",
+        'banner-email': "url('https://i.imgur.com/SZOyDSM.jpg')"
+
       })
     },
   },
